241cf5e91fcaef0a1355e0e87668a045c210d932
Ernest Wong <ewon521@gmail.com>
2016-12-22 19:48:59 -1200

    Modularize, added low pass filter, and now two gates

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <wiringPi.h>
 
-// Threshold values:
-//  - Direct: ?
-//  - Direct + Schmitt Trigger: ?
-//  - Pulldown: 5000
-//  - Pulldown + Schmitt Trigger: 0 
-#define FLICKER_THRESHOLD 0
+// Microseconds to wait before outputting data
+#define OUTPUT_PERIOD 1E5
 
+// What information do you want to log?
+//  - 1 = enable
+//  - 0 = disable
 #define DATALOG 1
 #define FLICKERLOG 0
+#define PINLOG 0
 
 #define datalog(...) \
   do { \
@@ -24,54 +25,133 @@
       printf(__VA_ARGS__); \
   } while (0);
 
-int main()
+#define pinlog(...) \
+  do { \
+    if (PINLOG) \
+      printf(__VA_ARGS__); \
+  } while (0);
+
+typedef struct
+Photogate
+{
+  int pin;
+  int raw;
+  int value;
+  int flickerThreshold;
+  unsigned int prevEdgeMicros;
+  unsigned int prevHighMicros;
+  int frames;
+  double timeDiff;
+  double freq;
+  double framePeriod;
+  double maxFreq;
+  double freqError;
+  double smoothing;
+  double freqSmoothed;
+}
+Photogate;
+
+// Threshold values:
+//  - Direct: ?
+//  - Direct + Schmitt Trigger: ?
+//  - Pulldown: 5000
+//  - Pulldown + Schmitt Trigger: 0
+
+Photogate *
+photogateInit(int pin, int flickerThreshold, double smoothing)
+{
+  Photogate * p = malloc(sizeof(Photogate));
+
+  pinMode(pin, INPUT);
+
+  p->pin = pin;
+  p->raw = digitalRead(pin);
+  p->value = digitalRead(pin);
+  p->flickerThreshold = flickerThreshold;
+  p->prevEdgeMicros = micros();
+  p->prevHighMicros = micros();
+
+  p->frames = 0;
+  p->timeDiff = 0;
+  p->freq = 0;
+  p->framePeriod = 0;
+  p->maxFreq = 0;
+  p->freqError = 0;
+
+  p->smoothing = smoothing;
+  p->freqSmoothed = 0;
+
+  return p;
+}
+
+void
+photogateUpdate(Photogate * p, unsigned int currMicros)
+{
+  int prev = p->raw;
+  int curr = digitalRead(p->pin);
+
+  // Flicker Edge
+  if (prev != curr)
+  {
+    int duration = currMicros - p->prevEdgeMicros;
+    flickerlog("Pin %d was %d for %d ms", p->pin, prev, duration);
+
+    // Criteria for a flicker that isn't noise
+    if (duration > p->flickerThreshold)
+    {
+      // Rising Edge
+      if (!p->value && curr)
+      {
+        p->value = 1;
+
+        p->timeDiff = currMicros - p->prevHighMicros;
+        p->timeDiff *= 1E-6;
+        p->freq = 1 / p->timeDiff;
+
+        p->framePeriod = p->timeDiff / p->frames;
+        p->maxFreq = 1 / (p->timeDiff - p->framePeriod);
+        p->freqError = p->maxFreq - p->freq;
+
+        double dt = p->timeDiff >= 1 ? 1 : 0;
+        p->freqSmoothed += (p->freq - p->freqSmoothed) * p->smoothing * dt;
+
+        p->frames = 0;
+        p->prevHighMicros = currMicros;
+      }
+
+      // Falling Edge
+      else if (p->value && !curr)
+      {
+        p->value = 0;
+      }
+    }
+
+    p->prevEdgeMicros = currMicros;
+  }
+  p->raw = curr;
+  p->frames++;
+}
+
+int
+main()
 {
   wiringPiSetup();
-  pinMode(0, INPUT);
-  unsigned int prevMicros = micros();
-  bool prev = false;
-  unsigned long long int frames = 0;
 
-  unsigned int prevChangeMicros = 0;
-  bool debouncedValue = false;
+  Photogate * greenGate = photogateInit(0, 0, 1);
+  Photogate * yellowGate = photogateInit(1, 0, 1);
 
-  printf("Just to let you know that this program is actually running ^_^\n");
+  unsigned int nextOutputMicros = micros();
 
   while (1)
   {
-    unsigned int currentMicros = micros();
-    bool current = digitalRead(0);
-    if (!prev && current)
+    unsigned int currMicros  = micros();
+    photogateUpdate(greenGate, currMicros);
+    photogateUpdate(yellowGate, currMicros);
+    pinlog("%d\t%d\n", greenGate->raw, yellowGate->raw);
+    if (nextOutputMicros < currMicros)
     {
-      unsigned int duration = currentMicros - prevChangeMicros;
-      flickerlog("LOW  for %d milliseconds.\n", duration);
-      if (duration > FLICKER_THRESHOLD && !debouncedValue)
-      {
-        debouncedValue = true;
-
-        double timeDifference = currentMicros - prevMicros;
-        timeDifference *= 1E-6;
-        double frequency = 1 / timeDifference;
-        double timePerFrame = timeDifference / frames;
-        double maxFrequency = 1 / (timeDifference - timePerFrame);
-        double frequencyError = maxFrequency - frequency;
-        prevMicros = currentMicros;
-        frames = 0;
-        datalog("Frequency: %f +/- %f\n", frequency, frequencyError);
-      }
-      prevChangeMicros = currentMicros;
+      nextOutputMicros = currMicros + OUTPUT_PERIOD;
+      datalog("%f\t%f\n", greenGate->freqSmoothed, yellowGate->freqSmoothed);
     }
-    else if (prev && !current)
-    {
-      unsigned int duration = currentMicros - prevChangeMicros;
-      flickerlog("HIGH for %d milliseconds.\n", duration);
-      if (duration > FLICKER_THRESHOLD && debouncedValue)
-      {
-        debouncedValue = false;
-      }
-      prevChangeMicros = currentMicros;
-    }
-    prev = current;
-    frames++;
   }
 }
